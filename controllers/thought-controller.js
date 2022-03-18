e518190daec781ce6955a06371c41cba6312d442
Matt <86723332+mattmc86@users.noreply.github.com>
2022-03-18 10:16:37 +0000

    Added initial file and folders and added pseudo code for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,12 @@
+const { Thoughts, User } = require("./models");
+const thoughtsController = {
+  //get all thoughts
+  //create thought
+  //get one thought by ID
+  //update a thought by ID
+  //delete a thought by ID
+  //add reaction
+  //delete reaction
+};
+
+module.exports = thoughtsController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,12 @@
+const { Thoughts, User } = require("./models");
+const userController = {
+  //get all users
+  //create user
+  //get a single user by ID and populated thought and friend data
+  //update user by ID
+  // delete user by ID
+  //add friend
+  //remove friend
+};
+
+module.exports = userController;
